f941d003d0c0a6933e937cf52b4df3f4c0e15680
frapp1e <107433806+frapp1e@users.noreply.github.com>
2022-06-15 22:32:09 +0200

    colliders pueblo paleta

--- Project_8_Solution/Project_8_Solution/Source/SceneLevel1.cpp
+++ Project_8_Solution/Project_8_Solution/Source/SceneLevel1.cpp
@@ -460,8 +460,8 @@ bool SceneLevel1::Start()
 	App->collisions->AddCollider({ 187, 0, 57, 10 }, Collider::Type::TORUTA1);
 
 	//Colliders muros
-	App->collisions->AddCollider({0, 0, 30, 300}, Collider::Type::WALL);
-	App->collisions->AddCollider({0, 300, 125, 100}, Collider::Type::WALL);
+	App->collisions->AddCollider({0, 0, 30, 294}, Collider::Type::WALL);
+	App->collisions->AddCollider({0, 294, 118, 105}, Collider::Type::WALL); //arboles de abajo
 	App->collisions->AddCollider({305, 355, 10, 40}, Collider::Type::WALL);
 	App->collisions->AddCollider({315, 345, 120, 50}, Collider::Type::WALL);
 	App->collisions->AddCollider({183, 382-6, 124, 24}, Collider::Type::WALL);
@@ -473,6 +473,27 @@ bool SceneLevel1::Start()
 	App->collisions->AddCollider({ 316,317, 47,19 }, Collider::Type::WALL);
 	App->collisions->AddCollider({ 398,0, 32,345 }, Collider::Type::WALL);
 	App->collisions->AddCollider({ 240,0, 158,47 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 30,0, 158,46 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 85,204, 69,25 }, Collider::Type::WALL); //valla con cartel izquierda
+	App->collisions->AddCollider({ 154,198, 18,30 }, Collider::Type::WALL); //cartel madera
+	App->collisions->AddCollider({ 118,293, 70,87 }, Collider::Type::WALL); //agua
+
+	//casa izquierda
+	App->collisions->AddCollider({ 124,45, 40,98 }, Collider::Type::WALL); //casa izquierda (part derecha)
+	App->collisions->AddCollider({ 163,128, 6,21 }, Collider::Type::WALL); //casa izquierda pico derecha
+	App->collisions->AddCollider({ 164,63, 13,72 }, Collider::Type::WALL); //casa izquierda derecha extremo
+	App->collisions->AddCollider({ 82,46, 42,79 }, Collider::Type::WALL); //casa izquierda parte izquierda
+	App->collisions->AddCollider({ 73,118, 18,31 }, Collider::Type::WALL); //casa izquierda buzon
+	App->collisions->AddCollider({ 91,125, 13,20 }, Collider::Type::WALL); //casa izquierda hueco
+
+	//casa derecha 
+	App->collisions->AddCollider({ 299,45, 40,98 }, Collider::Type::WALL); //casa derecha (part derecha)
+	App->collisions->AddCollider({ 337,128, 6,21 }, Collider::Type::WALL); //casa derecha pico derecha
+	App->collisions->AddCollider({ 256,47, 43,78 }, Collider::Type::WALL); //casa derecha izq
+	App->collisions->AddCollider({ 249,120, 19,27 }, Collider::Type::WALL); //casa derecha buzon
+	App->collisions->AddCollider({ 267,126, 12,19 }, Collider::Type::WALL); //casa derecha hueco
+	App->collisions->AddCollider({ 339,65, 15,68 }, Collider::Type::WALL); //casa derecha parte de la derecha extremo
+
 
 	
 
